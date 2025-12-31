cc61af1868ada91a4957edc5b6d2680aa1ad5bc2
behnam <behnam@behnams-MacBook-Pro.local>
2025-12-30 20:08:51 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { AppRegistry, Platform } from 'react-native';
+import ReactDOM from 'react-dom/client';
+import AppNavigator from './navigation/AppNavigator';
+
+// For React Native mobile
+AppRegistry.registerComponent('wagywebapp', () => AppNavigator);
+
+// For React Native Web
+if (Platform.OS === 'web') {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <AppNavigator />
+    </React.StrictMode>
+  );
+}
